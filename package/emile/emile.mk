ac4677f2a016b69874005b7a159ad291dec67802
Daniel Palmer <daniel@0x0f.com>
2025-02-24 14:51:49 +0900

    F

--- package/emile/emile.mk
+++ package/emile/emile.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-EMILE_VERSION = 59244addfc46ae9a5c96722c629c22ea229add57
+EMILE_VERSION = ee4b5feb4e443acbf4b8734e5c72914ef977431d
 #EMILE_SITE = https://github.com/vivier/EMILE.git
 EMILE_SITE = https://github.com/fifteenhex/EMILE.git
 EMILE_SITE_METHOD = git
 EMILE_LICENSE = GPLv2
+EMILE_MESON_EXTRA_BINARIES="objcopy=\'$(TARGET_OBJCOPY)\'"
 
 $(eval $(meson-package))
