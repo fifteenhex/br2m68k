4889d6eeb73a4465488c89502d5bd7832c6fed95
Daniel Palmer <daniel@0x0f.com>
2025-04-30 09:15:15 +0900

    pkg: Add bstoolbox

---
+++ package/bstoolbox/bstoolbox.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# bstoolbox
+#
+################################################################################
+
+BSTOOLBOX_VERSION = 3d38a598b143119a1090feef71ad5e6bbc16fec7
+BSTOOLBOX_SITE = https://github.com/fifteenhex/bstoolbox.git
+BSTOOLBOX_SITE_METHOD = git
+BSTOOLBOX_LICENSE = GPLv3
+
+$(eval $(meson-package))
